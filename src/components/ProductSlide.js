b543c0c20cf98d8158bad1eb257706cf4c2d312c
LP Boucher <lp_boucher@hotmail.com>
2019-06-03 23:56:11 +0200

    added toggles for forms

--- src/components/ProductSlide.js
+++ src/components/ProductSlide.js
@@ -16,16 +16,28 @@ class ProductSlide extends Component {
         super();
         this.toggleVideo = this.toggleVideo.bind(this);
         this.toggleFeatures = this.toggleFeatures.bind(this);
-        this.state = { collapseVideo: false, collapseFeatures: false };
+        this.toggleOrder = this.toggleOrder.bind(this);
+        this.toggleMeasure = this.toggleMeasure.bind(this);
+        this.state = { collapseVideo: false, collapseFeatures: false, collapseOrder: false, collapseMeasure: false };
     }
-    
+
+    //need to refactor into single multi-purpose function
     toggleVideo() {
         this.setState(state => ({ collapseVideo: !state.collapseVideo }));
     }
-
+    
     toggleFeatures() {
         this.setState(state => ({ collapseFeatures: !state.collapseFeatures }));
     }
+
+    toggleOrder() {
+        this.setState(state => ({ collapseOrder: !state.collapseOrder }));
+    }
+
+    toggleMeasure() {
+        this.setState(state => ({ collapseMeasure: !state.collapseMeasure }));
+    }
+
 
     render() {
     const { nom, caracteristiques, image } = this.props.item;
@@ -35,11 +47,11 @@ class ProductSlide extends Component {
             <div className="p-5" id="hands">
                 <Container fluid className="text-center p-5 bg-white">
                     <Row>
-                        <Col className="d-flex flex-column">
+                        <Col className="d-flex flex-column" style={{minHeight: '540px'}}>
                             <h3 className="mb-4">{nom}</h3>
                             <BackgroundImage className="flex-grow-1" fluid={image.fluid} />
                         </Col>
-                        <Col>
+                        <Col className="d-flex flex-column">
                             {caracToShow.map(car => 
                                 <Fragment key={car.id}>
                                     <ProductFeatureItem {...car} />
@@ -65,16 +77,26 @@ class ProductSlide extends Component {
                                     </Collapse>
                                 </Fragment>
                                 : null}
-                            <div className="my-2 d-flex justify-content-start align-items-center">
+                            <div className="flex-grow-1  my-2 d-flex justify-content-start align-items-end">
                                 <Button style={{backgroundColor: '#0074B4', borderColor: '#0074B4'}} className="rounded-0 px-3 mx-1" onClick={this.toggleVideo}>Measurement video</Button>
-                                <Button style={{backgroundColor: '#4A4A4A', borderColor: '#4A4A4A'}} className="rounded-0 px-3 mx-1">Measurement form</Button>
-                                <Button style={{backgroundColor: '#2A3849', borderColor: '#2A3849'}} className="rounded-0 px-3 mx-1">Order form</Button>
+                                <Button style={{backgroundColor: '#4A4A4A', borderColor: '#4A4A4A'}} className="rounded-0 px-3 mx-1" onClick={this.toggleMeasure}>Measurement form</Button>
+                                <Button style={{backgroundColor: '#2A3849', borderColor: '#2A3849'}} className="rounded-0 px-3 mx-1" onClick={this.toggleOrder}>Order form</Button>
                             </div>
                         </Col>
                     </Row>
                     <Collapse isOpen={this.state.collapseVideo}>
                         <Container fluid className="pt-5">
                             <iframe width="650" height="486" src={this.props.video} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </Container>
+                    </Collapse>
+                    <Collapse isOpen={this.state.collapseOrder}>
+                        <Container fluid className="pt-5">
+                            <p>Order</p>
+                        </Container>
+                    </Collapse>
+                    <Collapse isOpen={this.state.collapseMeasure}>
+                        <Container fluid className="pt-5">
+                            <p>Measure</p>
                         </Container>
                     </Collapse>
                 </Container>
