82b349b06746f04192ef02cec88f373821bf7a41
LP Boucher <lp_boucher@hotmail.com>
2019-09-25 10:10:58 +0200

    handle redirects to main domain

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,16 @@ const path = require(`path`)
 const languages = require('./src/data/languages')
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage, createRedirect } = actions;
+
+  createRedirect({ fromPath: 'https://recoverygarmentcenter.ca/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: 'https://recoverygarmentcenter.com/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: 'https://recoverygarmentcenter.net/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: 'https://recoverygarment.com/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: 'https://recoverygarment.ca/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: 'https://recovery-garment.com/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+  createRedirect({ fromPath: ' https://recovery-garment.ca/*', toPath: 'https://recoverygarmentcentre.ca/:splat', isPermanent: true, force: true })
+
   console.log(languages);
 
   const locales = languages.langs;
